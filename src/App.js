576fd5b0de4b9c58a83fd5766e261fd371c1af76
Cobbina Emmanuel <kobbyj099@gmail.com>
2023-02-23 12:53:16 +0000

    Modified

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
 import CardList from './CardList'
 import { robots } from './robots';
 import SearchBox from './SearchBox'
 
-const App = () => {
+class App extends Component {
+  state = {  } 
+  render() { 
     return (
-      <div>
-        <h1 className='text-uppercase text-center'>Robofriends</h1>
+         <div>
+        <h1 className='text-uppercase header text-center'>Robofriends</h1>
         <SearchBox/>
         <CardList robots={robots} />
       </div>
     );
+  }
 }
+ 
 export default App;
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -3,7 +3,7 @@ import React from 'react';
 const SearchBox = () => {
     return (
       <div className='text-center'>
-        <input type="search" placeholder="Search Robots" />
+        <input type="search" placeholder="Search Robots" className='bg-info'/>
       </div>
     );
 }
