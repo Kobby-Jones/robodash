9f5b5632460093767131c253f61373923ce21a2d
Cobbina Emmanuel <kobbyj099@gmail.com>
2023-02-23 13:02:08 +0000

    Searchbox

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() { 
     return (
          <div>
-        <h1 className='text-uppercase header text-center'>Robofriends</h1>
+        <h1 className='text-uppercase header mt-4 text-center'>Robofriends</h1>
         <SearchBox/>
         <CardList robots={robots} />
       </div>
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -3,7 +3,7 @@ import React from 'react';
 const SearchBox = () => {
     return (
       <div className='text-center'>
-        <input type="search" placeholder="Search Robots" className='bg-info'/>
+        <input type="search" placeholder="Search Robots" className='search-box rounded-2'/>
       </div>
     );
 }
